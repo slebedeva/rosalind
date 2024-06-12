3f957dad8cc000308325fda8550ea4f5fec6b5cb
Svetlana Lebedeva <slebedeva@gmail.com>
2024-06-12 18:17:14 +0200

    added package skeleton

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rosalind",
+    version="1.0.0",
+    author="Svetlana Lebedeva",
+    description="Basic functions to work with biological sequences.",
+    long_description=open("README.md").read(),
+    url="https://github.com/slebedeva/rosalind",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "rosalind = mypackage.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
